1d2e9ab9a23d5b99cca3f62154de24a17923f716
Chris Bailey <me@cbailey.co.uk>
2024-09-05 11:06:56 +0100

    Persist job completion in transaction. Handle concurrent access and unique constraint

--- lib/worker.ex
+++ lib/worker.ex
@@ -3,43 +3,60 @@ defmodule Monarch.Worker do
   Defines the Monarch Worker for running Oban jobs.
   """
 
-  use Oban.Worker
+  use Oban.Worker,
+    unique: [
+      fields: [:args, :worker],
+      keys: [:job, :repo]
+    ]
 
+  @impl Oban.Worker
   def perform(job) do
     worker = String.to_existing_atom(job.args["job"])
     repo = String.to_existing_atom(job.args["repo"])
 
     # Wraps a chunk of records to be updated in a transaction
     {:ok, action} =
-      if apply(worker, :skip, []) do
-        {:ok, :halt}
-      else
-        repo.transaction(fn ->
-          case apply(worker, :query, []) do
-            [] ->
-              :halt
+      cond do
+        apply(worker, :skip, []) ->
+          {:ok, :halt}
 
-            chunk ->
-              apply(worker, :update, [chunk])
-              :cont
-          end
-        end)
+        Monarch.completed?(repo, worker) ->
+          {:ok, :halt}
+
+        true ->
+          repo.transaction(fn ->
+            case apply(worker, :query, []) do
+              [] ->
+                :ok = log_completed!(repo, worker)
+                {:ok, :halt}
+
+              chunk ->
+                apply(worker, :update, [chunk])
+                {:ok, :cont}
+            end
+          end)
       end
 
     # Recursively perform the Oban job if there are still records to be updated.
     if action == :halt do
-      {1, _} =
-        repo.insert_all("monarch_jobs", [
-          %{
-            inserted_at:
-              DateTime.utc_now() |> DateTime.to_naive() |> NaiveDateTime.truncate(:second),
-            name: to_string(worker)
-          }
-        ])
-
       :ok
     else
       perform(job)
     end
+  end
+
+  defp log_completed!(repo, worker) do
+    {1, _} =
+      repo.insert_all("monarch_jobs", [
+        %{
+          inserted_at:
+            DateTime.utc_now()
+            |> DateTime.to_naive()
+            |> NaiveDateTime.truncate(:second),
+          name: to_string(worker)
+        }
+      ])
+
+    :ok
   end
 end
