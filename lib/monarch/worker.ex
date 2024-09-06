30326d6e5876f233b0c6e73dff12272121e41110
Chris Bailey <me@cbailey.co.uk>
2024-09-06 11:07:29 +0100

    Move files into proper directory

--- lib/monarch/worker.ex
+++ lib/monarch/worker.ex
@@ -14,34 +14,52 @@ defmodule Monarch.Worker do
     worker = String.to_existing_atom(job.args["job"])
     repo = String.to_existing_atom(job.args["repo"])
 
-    # Wraps a chunk of records to be updated in a transaction
-    {:ok, action} =
+    {:ok, {action, resp}} =
       cond do
         apply(worker, :skip, []) ->
-          {:ok, :halt}
+          {:ok, {:halt, []}}
+
+        function_exported?(worker, :snooze?, 0) && apply(worker, :snooze?, []) ->
+          {:ok, {:snooze, apply(worker, :snooze?, [])}}
 
         Monarch.completed?(repo, worker) ->
-          {:ok, :halt}
+          {:ok, {:halt, []}}
 
         true ->
           maybe_transaction(worker, repo, fn ->
             case apply(worker, :query, []) do
               [] ->
                 :ok = log_completed!(repo, worker)
-                :halt
+                {:halt, []}
 
               chunk ->
-                apply(worker, :update, [chunk])
-                :cont
+                if is_list(job.args["chunk"]) && job.args["chunk"] == chunk do
+                  {:discard, chunk}
+                else
+                  apply(worker, :update, [chunk])
+                  {:cont, chunk}
+                end
             end
           end)
       end
 
-    # Recursively perform the Oban job if there are still records to be updated.
-    if action == :halt do
-      :ok
-    else
-      perform(job)
+    case action do
+      # Job finished successfully.
+      :halt ->
+        :ok
+
+      # Recursively perform the Oban job if there are still records to be updated.
+      # TODO: should we re-enqueue this working so it runs in a different pod?
+      #       if so we'll have to do this differently.
+      :cont ->
+        perform(%{job | args: Map.put(job.args, "chunk", resp)})
+
+      # Cycle was detected, no point in continuing.
+      :discard ->
+        {:discard, "Cycle detected. Discarding."}
+
+      :snooze ->
+        {:snooze, resp}
     end
   end
 
