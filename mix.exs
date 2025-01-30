68e95f4897955579b8edd82eaf6e3a58f5539b07
Ivy Markwell <me@ivymarkwell.com>
2025-01-30 17:53:21 -0600

    Use String.to_atom/1 instead of String.to_existing_atom/1 (#9)
    * just use String.to_atom
    
    * bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Monarch.MixProject do
   def project do
     [
       app: :monarch,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.17.2",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
